2b6ff1ffe07c49641aeaf847612746825c4addd1
Daniel M <dan7.9@hotmail.es>
2018-05-17 13:54:25 +0100

    Iris with total error

--- nn_iris.py
+++ nn_iris.py
@@ -13,27 +13,40 @@ def one_hot(x, n):
     if type(x) == list:
         x = np.array(x)
     x = x.flatten()
-    o_h = np.zeros((len(x), n))
-    o_h[np.arange(len(x)), x] = 1
+    o_h = np.zeros((len(x), n))  # raw, column
+    o_h[np.arange(len(x)), x] = 1  # put 1 in x column, i think
     return o_h
 
 
 data = np.genfromtxt('iris.data', delimiter=",")  # iris.data file loading
 np.random.shuffle(data)  # we shuffle the data
-x_data = data[:, 0:4].astype('f4')  # the samples are the four first rows of data
-y_data = one_hot(data[:, 4].astype(int), 3)  # the labels are in the last row. Then we encode them in one hot code
+# x_data = data[:, 0:4].astype('f4')  # the samples are the four first rows of data
+# y_data = one_hot(data[:, 4].astype(int), 3)  # the labels are in the last row. Then we encode them in one hot code
+#
+# print "\nSome samples..."
+# for i in range(20):
+#     print x_data[i], " -> ", y_data[i]
 
-print "\nSome samples..."
-for i in range(20):
-    print x_data[i], " -> ", y_data[i]
-print
+data70 = int(len(data) * 0.7)
+data85 = int(len(data) * 0.85)
+
+x_data_training = data[:data70, 0:4].astype('f4')
+y_data_training = one_hot(data[:data70, 4].astype(int), 3)
+
+x_data_validation = data[data70:data85, 0:4].astype('f4')
+y_data_validation = one_hot(data[data70:data85, 4].astype(int), 3)
+
+x_data_test = data[data85:, 0:4].astype('f4')
+y_data_test = one_hot(data[data85:, 4].astype(int), 3)
 
 x = tf.placeholder("float", [None, 4])  # samples
 y_ = tf.placeholder("float", [None, 3])  # labels
 
+# 4 entries, 5 neurons
 W1 = tf.Variable(np.float32(np.random.rand(4, 5)) * 0.1)
 b1 = tf.Variable(np.float32(np.random.rand(5)) * 0.1)
 
+# 5 entries, 3 neurons
 W2 = tf.Variable(np.float32(np.random.rand(5, 3)) * 0.1)
 b2 = tf.Variable(np.float32(np.random.rand(3)) * 0.1)
 
@@ -45,25 +58,40 @@ loss = tf.reduce_sum(tf.square(y_ - y))
 
 train = tf.train.GradientDescentOptimizer(0.01).minimize(loss)  # learning rate: 0.01
 
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init)
 
-print "----------------------"
-print "   Start training...  "
-print "----------------------"
+print("----------------------")
+print("   Start training...  ")
+print("----------------------")
 
 batch_size = 20
 
-for epoch in xrange(100):
-    for jj in xrange(len(x_data) / batch_size):
-        batch_xs = x_data[jj * batch_size: jj * batch_size + batch_size]
-        batch_ys = y_data[jj * batch_size: jj * batch_size + batch_size]
+for epoch in range(100):
+    for jj in range(len(x_data_training) // batch_size):  # Este bucle se ejecuta 5
+        batch_xs = x_data_training[jj * batch_size: jj * batch_size + batch_size]
+        batch_ys = y_data_training[jj * batch_size: jj * batch_size + batch_size]
         sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
 
-    print "Epoch #:", epoch, "Error: ", sess.run(loss, feed_dict={x: batch_xs, y_: batch_ys})
-    result = sess.run(y, feed_dict={x: batch_xs})
-    for b, r in zip(batch_ys, result):
-        print b, "-->", r
-    print "----------------------------------------------------------------------------------"
+    # validation
+    print("Epoch #:", epoch, "Error: ", sess.run(loss, feed_dict={x: batch_xs, y_: batch_ys}))
+
+    sess.run(y, feed_dict={x: x_data_validation})
+    result = sess.run(y, feed_dict={x: x_data_validation})
+    for b, r in zip(y_data_validation, result):
+        print(b, "-->", r)
+    print("----------------------------------------------------------------------------------")
+
+print("------------------")
+print("   Start test...  ")
+print("------------------")
+
+error = 0.
+result = sess.run(y, feed_dict={x: x_data_test})
+for b, r in zip(y_data_test, result):
+    print(b, "-->", r)
+    if (np.argmax(b) != np.argmax(r)):
+        error += 1
+print('Error: %.2f%%' % (error / len(result)))
--- nn_mnist.py
+++ nn_mnist.py
@@ -1,5 +1,5 @@
 import gzip
-import cPickle
+import pickle as cPickle
 
 import tensorflow as tf
 import numpy as np
@@ -22,20 +22,39 @@ def one_hot(x, n):
 
 
 f = gzip.open('mnist.pkl.gz', 'rb')
-train_set, valid_set, test_set = cPickle.load(f)
+# 50000, 10000, 10000 len
+train_set, valid_set, test_set = cPickle.load(f, encoding='iso-8859-1')
 f.close()
 
 train_x, train_y = train_set
-
+train_y = one_hot(train_y, 10)
+valid_x, valid_y = valid_set
+valid_y = one_hot(valid_y, 10)
+test_x, test_y = test_set
+test_y = one_hot(test_y, 10)
 
 # ---------------- Visualizing some element of the MNIST dataset --------------
 
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
-plt.imshow(train_x[57].reshape((28, 28)), cmap=cm.Greys_r)
-plt.show()  # Let's see a sample
-print train_y[57]
-
+# plt.imshow(train_x[56].reshape((28, 28)), cmap=cm.Greys_r)
+# plt.show()  # Let's see a sample
+# print(train_y[56])
 
 # TODO: the neural net!!
+# x = tf.placeholder("float", [None, 784])  # samples 28x28 pixels
+# y_ = tf.placeholder("float", [None, 10])  # labels  numbers
+
+
+import tensorflow as tf
+
+x = tf.placeholder("float", None)
+y = x * 2
+
+with tf.Session() as session:
+    result = session.run(y, feed_dict={x: [1, 2, 3]})
+    result = session.run(y, feed_dict={x: [4, 5, 6]})
+    print(result)
+
+print(result)
